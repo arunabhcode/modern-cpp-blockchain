21fd7af9f39e164cd047e211fd27ee8826f6309c
Arunabh Sharma <arunabh.1990@gmail.com>
2021-06-14 16:30:02 -0400

    add leveldb wrapper class

---
+++ modules/blockchain/include/mcb/db.h
@@ -0,0 +1,35 @@
+// Copyright 2021 Arunabh Sharma
+
+#ifndef MODULES_BLOCKCHAIN_INCLUDE_MCB_DB_H_
+#define MODULES_BLOCKCHAIN_INCLUDE_MCB_DB_H_
+
+#include <memory>
+#include <string>
+
+#include "leveldb/db.h"
+
+namespace mcb
+{
+class DbWrapper
+{
+ public:
+  DbWrapper() = delete;
+  explicit DbWrapper(const std::string& db_dir);
+  ~DbWrapper();
+
+  bool OpenDatabase();
+  void CloseDatabase();
+
+  bool Write(const std::string& key, const std::string& value);
+  bool Read(const std::string& key, std::string& value);
+  bool Remove(const std::string& key);
+
+ private:
+  leveldb::DB* db_;
+  leveldb::Options options_;
+  std::string db_dir_;
+};
+
+}  // namespace mcb
+
+#endif  // MODULES_BLOCKCHAIN_INCLUDE_MCB_DB_H_
---
+++ modules/blockchain/src/db.cpp
@@ -0,0 +1,54 @@
+// Copyright 2021 Arunabh Sharma
+
+#include "mcb/db.h"
+
+#include <exception>
+#include <system_error>
+
+namespace mcb
+{
+DbWrapper::DbWrapper(const std::string& db_dir) : db_dir_(db_dir)
+{
+  options_.create_if_missing = true;
+  if (!OpenDatabase())
+  {
+    std::error_code ec = make_error_code(std::errc::io_error);
+    throw std::system_error(ec, std::string("Opening database failed").c_str());
+  }
+}
+
+DbWrapper::~DbWrapper()
+{
+  CloseDatabase();
+}
+
+bool DbWrapper::OpenDatabase()
+{
+  leveldb::Status s = leveldb::DB::Open(options_, db_dir_, &db_);
+  return s.ok();
+}
+
+void DbWrapper::CloseDatabase()
+{
+  delete db_;
+}
+
+bool DbWrapper::Write(const std::string& key, const std::string& value)
+{
+  leveldb::Status s = db_->Put(leveldb::WriteOptions(), key, value);
+  return s.ok();
+}
+
+bool DbWrapper::Read(const std::string& key, std::string& value)
+{
+  leveldb::Status s = db_->Get(leveldb::ReadOptions(), key, &value);
+  return s.ok();
+}
+
+bool DbWrapper::Remove(const std::string& key)
+{
+  leveldb::Status s = db_->Delete(leveldb::WriteOptions(), key);
+  return s.ok();
+}
+
+}  // namespace mcb
